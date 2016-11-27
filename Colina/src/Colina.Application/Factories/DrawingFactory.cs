578fed18d7268b7616f51986827962957f5d3824
cassianomendes <cassianomendes.info@gmail.com>
2016-11-27 17:12:08 -0200

    drawing factory

--- Colina/src/Colina.Application/Factories/DrawingFactory.cs
+++ Colina/src/Colina.Application/Factories/DrawingFactory.cs
@@ -1,62 +1,61 @@
-﻿using Colina.Models.Abstraction.Designs;
-using System;
-using Actions = Colina.Models.Abstraction.Actions;
+﻿using System;
+using Colina.Models.Abstraction.Actions;
 
 namespace Colina.Language.Factories
 {
     public static class DrawingFactory
     {
-        public static Drawing Create(Actions.UserAction userAction)
+        public static Models.Abstraction.Designs.Drawing Create(UserAction userAction)
         {
             if (userAction == null)
                 throw new ArgumentNullException(nameof(userAction));
 
             var position = GetPosition(userAction);
 
-            return new Drawing(position, userAction.Object);
+            return new Models.Abstraction.Designs.Drawing(position, userAction.Object);
         }
 
-        private static Position GetPosition(Actions.UserAction userAction)
+        private static Models.Abstraction.Designs.Position GetPosition(UserAction userAction)
         {
             if (userAction.Position == null)
-                return new Position(0, 0);
+                return new Models.Abstraction.Designs.Position(0, 0);
 
-            if (userAction.Position is Actions.RelativePosition)
-                return GetRelativePosition(userAction.Position as Actions.RelativePosition);
-            else if (userAction.Position is Actions.AbsolutePosition)
-                return GetAbsolutePosition(userAction.Position as Actions.AbsolutePosition);
+            if (userAction.Position is RelativePosition)
+                return GetRelativePosition(userAction.Position as RelativePosition);
+            else if (userAction.Position is AbsolutePosition)
+                return GetAbsolutePosition(userAction.Position as AbsolutePosition);
             else
                 throw new ArgumentException(nameof(userAction));
         }
 
-        private static Position GetRelativePosition(Actions.RelativePosition position)
+        private static Models.Abstraction.Designs.Position GetRelativePosition(RelativePosition position)
         {
             throw new NotImplementedException();
         }
 
-        private static Position GetAbsolutePosition(Actions.AbsolutePosition position)
+        private static Models.Abstraction.Designs.Position GetAbsolutePosition(AbsolutePosition position)
         {
             throw new NotImplementedException();
 
             switch (position.Direction)
             {
-                //case Actions.Direction.Back:
+                //case Direction.Back:
 
                 //    break;
 
-                case Actions.Direction.Center:
+                case Direction.Center:
 
                     break;
 
-                case Actions.Direction.Down:
+                case Direction.Down:
 
                     break;
 
-                //case Actions.Direction.Front:
+                //case Direction.Front:
 
                 //    break;
 
-                case Actions.Direction.Up:
+                case Direction.Up:
                     
                     break;
 
